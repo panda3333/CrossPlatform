cfdefe240781cb1ac85e816b2c7c7e050a124c72
Brandon <b.shega@gmail.com>
2014-07-09 19:33:49 -0400

    Initial Commit of Xcode project

---
+++ iOS App/cpmios/cpmios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cpmios
+//
+//  Created by Brandon Shega on 7/9/14.
+//  Copyright (c) 2014 Brandon Shega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
