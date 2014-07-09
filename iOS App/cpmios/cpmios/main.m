dc75318d773120d9fcfdf5f2d89d6788177b16bf
Brandon <b.shega@gmail.com>
2014-07-09 19:58:00 -0400

    remade app

---
+++ iOS App/cpmios/cpmios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cpmios
+//
+//  Created by Brandon Shega on 7/9/14.
+//  Copyright (c) 2014 Brandon Shega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
